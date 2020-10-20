6134638a5e33a31ab73d12899457b7571a9c7138
Simon Groom <simon_groom@outlook.com>
2020-10-20 10:13:16 -0400

    product_detail: fix overflow bug, use new product save function, and add field for nextBatchNumber

--- lib/product_detail.dart
+++ lib/product_detail.dart
@@ -1,5 +1,4 @@
 import 'package:batcher/models/product.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
@@ -17,8 +16,6 @@ class ProductDetail extends StatefulWidget {
 }
 
 class _ProductDetailState extends State<ProductDetail> {
-  final CollectionReference products =
-      FirebaseFirestore.instance.collection('products');
   final Product product;
 
   _ProductDetailState({
@@ -28,99 +25,121 @@ class _ProductDetailState extends State<ProductDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              keyboardType: TextInputType.text,
-              initialValue: product.clientName,
-              onChanged: (val) {
-                product.clientName = val;
-              },
-              decoration: const InputDecoration(
-                labelText: 'Client Name',
-                hintText: 'Client Name',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                keyboardType: TextInputType.text,
+                initialValue: product.clientName,
+                onChanged: (val) {
+                  product.clientName = val;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Client Name',
+                  hintText: 'Client Name',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter the client name';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter the client name';
-                }
-                return null;
-              },
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              keyboardType: TextInputType.text,
-              initialValue: product.productName,
-              onChanged: (val) {
-                product.productName = val;
-              },
-              decoration: const InputDecoration(
-                labelText: 'Product Name',
-                hintText: 'Product Name',
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                keyboardType: TextInputType.text,
+                initialValue: product.productName,
+                onChanged: (val) {
+                  product.productName = val;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Product Name',
+                  hintText: 'Product Name',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter the product name';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter the product name';
-                }
-                return null;
-              },
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              keyboardType: TextInputType.number,
-              initialValue: product.productCode.toString(),
-              onChanged: (val) {
-                product.productCode = int.parse(val);
-              },
-              decoration: const InputDecoration(
-                labelText: 'Product Code',
-                hintText: 'Product Code',
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                initialValue: product.productCode.toString(),
+                onChanged: (val) {
+                  product.productCode = int.parse(val);
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Product Code',
+                  hintText: 'Product Code',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter the product code';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter the product code';
-                }
-                return null;
-              },
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextFormField(
-              keyboardType: TextInputType.number,
-              initialValue: product.shelfLife.toString(),
-              onChanged: (val) {
-                product.shelfLife = int.parse(val);
-              },
-              decoration: const InputDecoration(
-                labelText: 'Shelf Life (months)',
-                hintText: 'Shelf Life (months)',
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                initialValue: product.shelfLife.toString(),
+                onChanged: (val) {
+                  product.shelfLife = int.parse(val);
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Shelf Life (months)',
+                  hintText: 'Shelf Life (months)',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter the shelf life';
+                  }
+                  return null;
+                },
               ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter the shelf life';
-                }
-                return null;
-              },
             ),
-          ),
-          SwitchListTile(
-            title: const Text('Cold Fill'),
-            value: product.isColdFill,
-            onChanged: (bool value) {
-              setState(() {
-                product.isColdFill = value;
-              });
-            },
-            secondary: const Icon(Icons.thermostat_sharp),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                keyboardType: TextInputType.number,
+                initialValue: product.nextBatchNumber?.toString(),
+                onChanged: (val) {
+                  product.nextBatchNumber = int.parse(val);
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Next Batch Number',
+                  hintText: 'Next Batch Number',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter the next batch number';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SwitchListTile(
+              title: const Text('Cold Fill'),
+              value: product.isColdFill,
+              onChanged: (bool value) {
+                setState(() {
+                  product.isColdFill = value;
+                });
+              },
+              secondary: const Icon(Icons.thermostat_sharp),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: "Save",
@@ -130,7 +149,7 @@ class _ProductDetailState extends State<ProductDetail> {
               // this is a new product, create an id
               product.productId = Uuid().v4();
             }
-            products.doc(product.productId).set(product.toJson());
+            product.save();
           });
         },
         child: Icon(
