7de8c990ed5e20f5e269b14bac61a31adc3e0df5
Simon Groom <simon_groom@outlook.com>
2020-11-18 11:40:58 -0500

    product_list: refactor to work for a single client

--- lib/product_list.dart
+++ lib/product_list.dart
@@ -1,19 +1,29 @@
 import 'package:batcher/models/product.dart';
-import 'package:batcher/product_search.dart';
+import 'package:batcher/models/client.dart';
 import 'package:batcher/product_view.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ProductList extends StatefulWidget {
+  final Client client;
+
+  const ProductList({Key key, this.client}) : super(key: key);
+
   @override
-  _ProductListState createState() => _ProductListState();
+  _ProductListState createState() => _ProductListState(client);
 }
 
 class _ProductListState extends State<ProductList> {
+  final Client client;
+
   final CollectionReference products =
       FirebaseFirestore.instance.collection('products');
 
   List<Product> productList = [];
+
+  _ProductListState(
+    this.client,
+  );
 
   @override
   void initState() {
@@ -22,12 +32,13 @@ class _ProductListState extends State<ProductList> {
   }
 
   void getProductList() async {
-    QuerySnapshot _products = await products.get();
+    QuerySnapshot _products =
+        await products.where("client_id", isEqualTo: client.clientId).get();
     setState(() {
       productList = _products.docs.map((p) {
         return Product.fromJson(p.data());
       }).toList();
-      productList.sort((a, b) => a.clientName.compareTo(b.clientName));
+      productList.sort((a, b) => a.productName.compareTo(b.productName));
     });
   }
 
@@ -35,50 +46,48 @@ class _ProductListState extends State<ProductList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {
-              showSearch(
-                  context: context,
-                  delegate: ProductSearch(
-                    productList: productList,
-                  ));
-            },
-            icon: Icon(Icons.search),
-          )
-        ],
-        title: Text('Search Products'),
-        backgroundColor: Colors.grey[400],
+        title: Text(client.clientName),
       ),
-      body: ListView.builder(
-        itemCount: productList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ProductView(
-                      product: productList[index],
+      body: productList.length == 0
+          ? Center(
+              child: Text(
+                "No Products - tap the + button to add one",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            )
+          : ListView.builder(
+              itemCount: productList.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProductView(
+                            product: productList[index],
+                            client: client,
+                          ),
+                        ),
+                      ).then((value) {
+                        setState(() {
+                          getProductList();
+                        });
+                      });
+                    },
+                    splashColor: Colors.blue.withAlpha(30),
+                    child: ListTile(
+                      title: Text(productList[index].productName),
+                      // subtitle: ,
+                      trailing: Icon(Icons.arrow_forward),
                     ),
                   ),
-                ).then((value) {
-                  setState(() {
-                    getProductList();
-                  });
-                });
+                );
               },
-              splashColor: Colors.blue.withAlpha(30),
-              child: ListTile(
-                title: Text(productList[index].clientName),
-                subtitle: Text(productList[index].productName),
-                trailing: Icon(Icons.arrow_forward),
-              ),
             ),
-          );
-        },
-      ),
       floatingActionButton: FloatingActionButton(
         tooltip: "Add Product",
         child: Icon(Icons.add),
@@ -91,6 +100,7 @@ class _ProductListState extends State<ProductList> {
             MaterialPageRoute(
               builder: (context) => ProductView(
                 product: _product,
+                client: client,
               ),
             ),
           ).then((value) {
